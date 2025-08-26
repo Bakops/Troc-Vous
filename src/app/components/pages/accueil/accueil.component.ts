c129194c0346d444fefb6b23457f1234cfaf5661
Bakops <bakoutoure4@gmail.com>
2025-08-26 18:58:18 +0200

    Création HEro-Header

--- src/app/components/pages/accueil/accueil.component.ts
+++ src/app/components/pages/accueil/accueil.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../../layout/footer/footer.component';
 import { HeaderComponent } from '../../layout/header/header.component';
+import { HeroHeaderComponent } from '../../layout/hero-header/hero-header.component';
 
 @Component({
   selector: 'app-accueil',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent, HeroHeaderComponent],
   templateUrl: './accueil.component.html',
   styleUrl: './accueil.component.css',
 })
