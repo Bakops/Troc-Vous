9810da4954fc47679e0e2c983dd361dcdc5fac69
Bakops <bakoutoure4@gmail.com>
2025-09-14 15:56:35 +0200

    Ajoute slider animé + Card statique

---
+++ src/app/components/layout/card-app/card-app.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-app',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './card-app.component.html',
+  styleUrl: './card-app.component.css',
+})
+export class CardAppComponent {
+  isVisible: boolean = true;
+
+  closeCard() {
+    this.isVisible = false;
+  }
+}
--- src/app/components/pages/accueil/accueil.component.ts
+++ src/app/components/pages/accueil/accueil.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CardAppComponent } from '../../layout/card-app/card-app.component';
 import { FooterComponent } from '../../layout/footer/footer.component';
 import { HeaderComponent } from '../../layout/header/header.component';
 import { HeroHeaderComponent } from '../../layout/hero-header/hero-header.component';
-
+import { SliderComponent } from '../../layout/slider/slider.component';
 @Component({
   selector: 'app-accueil',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, HeroHeaderComponent],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    HeroHeaderComponent,
+    SliderComponent,
+    CardAppComponent,
+  ],
   templateUrl: './accueil.component.html',
   styleUrl: './accueil.component.css',
 })
